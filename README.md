d8ff524700c878cfe1955e653a6205cb4f56dc79
Arjun Gupta <arjungup740@gmail.com>
2024-08-12 10:27:32 -0400

    flesh out notebook

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
-Note that 2024 is misisng DC, Wisconsin, and Wyoming behavior
-
-There's various stores we're missing in the error dict and various stores probably missing betwene 23 and 24, can go back and do a comparison
-
-Data issues to understand
-* missing most states & cities
-    * do a catalog of what's missing
 
 What we are seeking to understand
-* how much more expensive is taco bell in new york vs. Charlotte? Greenville? middle of nowhere alabama?
-    * what is the average and median price of a given menu item w/in a city
-* what is the distribution of menu item prices within cities/MSAs vs across them?
+* how much spread there 
+* how much more expensive is taco bell in places that are expensive to live vs. places that are cheap to live -- cities/metro areas vs. everything else, and across states
 * how have these changed since 2023?
-    
+* how does your locale compare to the national average
+
+
+
+########### thoughts dump
+
+Overall findings:
+* prices are remarkably tight, and don't vary drastically on core menu items like crunchwrap supreme
+* spread a big bigger on higher priced items
+* less variation across taco bell vs. cost of living indices, e.g. taco bell 
+
+BACKLOG
+* debug why we only have a small amount of states
+* try MSA aggregations: https://github.com/chambliss/Major-Metro-Areas-And-Their-Cities
+    * compare the micropolitan to the metropolitan
+* matrix of pct differences
+* compare the 5 boroughs to each other
+* compare 5 boroughs vs. charlotte, other places
+* compare top 20 MSAs vs. rest of the country
+* compare how results track against general cost of living indices for the same places, or general population things
+* think on ways for an app to display all the data
+
+
 Considerations to add/think about after first pass together
 * pct differences below a certain threshold don't matter
 * if only 1 or two restos vs. many
 
-TODO:
-* pick a couple menu items to focus on
-* build state comparisons across core menu items
-* city comparisons across core menu items
-* pull MSA aggregations
-* debug why we only have a small amount of states
+Data issues to understand
+* missing most states & cities
+    * do a catalog of what's missing
+* See how many times your num appearance on menus for common items differs from number of stores in the city
+
+Note that 2024 is misisng DC, Wisconsin, and Wyoming behavior
+
+There's various stores we're missing in the error dict and various stores probably missing betwene 23 and 24, can go back and do a comparison
--- analyze.py
+++ analyze.py
@@ -1,4 +1,15 @@
+import pandas as pd
+import pickle as pkl
+import matplotlib.pyplot as plt
+import seaborn as sns
+pd.options.display.float_format = '{:.2f}'.format
+
+## some pkl read in
+with open('full_df.pkl', 'rb') as file:
+    full_df = pkl.load(file)
+
 len(full_df)
+
 ## dedupe menu items
 full_df = full_df.drop_duplicates(subset = ['item_name', 'store_id']) # need this because the same item might be listed under multiple categories
 len(full_df)
@@ -10,27 +21,63 @@ full_df.columns
 
 full_df['state'], full_df['city'] = zip(*full_df['location_url'].str.split('/').str[3:5])
 
-locations_count_by_city = full_df.groupby(['state', 'city'])['location_url'].nunique().reset_index().rename(columns = {'location_url':'count'})
-locations_count_by_city[locations_count_by_city['count'] != 1]
-comparison_df = full_df.groupby(['state', 'city', 'item_name'])['value']\
-                       .agg(mean='mean', median = 'median', num_obs='size', nunique = 'nunique', max = 'max', min = 'min')\
-                       .reset_index() # https://stackoverflow.com/questions/12589481/multiple-aggregations-of-the-same-column-using-pandas-groupby-agg
+core_items = [
+              'Supreme Taco Party Pack', 
+            #   'Chicken Quesadilla', 
+            #   'cinnabon delights 12 pack', 
+              'Crunchwrap Supreme®', 
+            #   'beefy 5 layer burrito', 
+            #   'Nacho cheese doritos locos taco', 
+            #   '3 doritos locos tacos combo'
+              ]
 
-state_comparison_df = full_df.groupby(['state', 'item_name'])['value']\
-                       .agg(mean='mean', median = 'median', num_obs='size', nunique = 'nunique', max = 'max', min = 'min')\
-                       .reset_index()
+working = full_df[full_df['item_name'].isin(core_items)]
+
+############################################### Do some digging
+
+agg_funcs = {
+    'mean': 'mean',
+    'median': 'median',
+    'num_stores': 'size',
+    'nunique_prices': 'nunique',
+    'max': 'max',
+    'min': 'min',
+    'sd': 'std'
+}
+
+########## in aggregate
 
 metric = 'median'
-# metric = 'mean'
+metric = 'mean'
+
+working .groupby('state')['value'].agg(**agg_funcs)
+
+working.groupby(['state', 'item_name'])['value'].agg(**agg_funcs).sort_values(by = ['item_name', metric], ascending = False)
+
+
+########## breaking down by menu items
+
+comparison_df = working.groupby(['state', 'city', 'item_name'])['value']\
+                       .agg(**agg_funcs)\
+                       .reset_index()\
+                       .sort_values(by = ['item_name', metric, 'state', 'city'], ascending = False) # https://stackoverflow.com/questions/12589481/multiple-aggregations-of-the-same-column-using-pandas-groupby-agg
+
+state_comparison_df = working .groupby(['state', 'item_name'])['value']\
+                       .agg(**agg_funcs)\
+                       .reset_index()\
+                       .sort_values(by = ['item_name', metric, 'state'], ascending = False)
+
 easy_compare = pd.pivot_table(comparison_df, columns = ['city', 'state'], index = 'item_name', values= metric).reset_index()
 easy_compare_states = pd.pivot_table(state_comparison_df, columns = ['state'], index = 'item_name', values = metric).reset_index()
- 
+
+city_list = [tuple(x) for x in working [['city', 'state']].drop_duplicates().values] 
+
 # city1 = ('charlotte', 'nc')
 # city1 = ('hollywood', 'fl')
 # city1 = ('huntsville', 'al')
-city1 =('alamo', 'ca')
+city1 =('mountain-view', 'ca')
 easy_compare[city1]
-city2 = ('adel', 'ga')
+city2 = ('greenville', 'al')
 # city2 = ('brooklyn', 'ny')
 two_city_compare = easy_compare[[('item_name',''), city1, city2]].dropna().copy()
 two_city_compare['delta'] = two_city_compare[city1] - two_city_compare[city2]
@@ -43,24 +90,34 @@ two_city_compare.select_dtypes(include='number').median()
 two_city_compare[ (two_city_compare['pct_delta'] <= -.2) | (two_city_compare['pct_delta'] >= .2)].sort_values(by = 'pct_delta', ascending = True)
 two_city_compare[two_city_compare['item_name'].str.contains('taco', case=False)].sort_values(by = 'pct_delta', ascending = True)
 
-full_df[(full_df['item_name'] == 'Watermelon Berry Lemonade Freeze') & (full_df['store_id'] == '038899')]
+working [(working ['item_name'] == 'Watermelon Berry Lemonade Freeze') & (working ['store_id'] == '038899')]
+
+
+###### distributions
+
+percentiles = [0, 10, 25, 50, 75, 90, 100]
+
+# Create a DataFrame to hold percentile values
+percentile_df = full_df.groupby(['state', 'item_name'])['value'].quantile([p/100 for p in percentiles])\
+                       .unstack()\
+                       .reset_index()\
+                       .sort_values(by = ['item_name', 1, 'state'], ascending = False)
+
+#### can stick in some histograms
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='group', y='value', data=percentile_df)
+plt.xticks(rotation=90)
+plt.xlabel('Group')
+plt.ylabel('Value')
+plt.title('Percentile Distributions by Group')
+plt.show()
+
 ############################################################################### data qa
 
 ## check if dupe menu items per store
-count_table = full_df.groupby(['item_name', 'store_id']).size().reset_index().rename(columns={0:'count'})
+count_table = working .groupby(['item_name', 'store_id']).size().reset_index().rename(columns={0:'count'})
 count_table[count_table['count']>1]
 
-############################################################################### In development
+locations_count_by_city = working .groupby(['state', 'city'])['location_url'].nunique().reset_index().rename(columns = {'location_url':'count'})
+locations_count_by_city[locations_count_by_city['count'] != 1]
 
-##### get all states in the union
-
-soup = get_soup(url)
-
-directory_links = soup.findAll('a', class_='Directory-listLink', limit = None)
-desired_keys = ['href', 'data-count']
-list_of_city_urls = [{key: link[key] for key in desired_keys} for link in directory_links]
-
-city_urls_df = pd.DataFrame(list_of_city_urls).rename(columns = {'data-count':'data_count', 'href':'url'})
-city_urls_df['data_count'] = city_urls_df['data_count'].str.replace('(', '').str.replace(')', '').apply(int)
-city_urls_df['url'] = 'https://locations.tacobell.com/' + city_urls_df['url']
- 
